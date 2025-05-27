4c9fd856d91ebb18ebe81ac66f4e695e27d70c3f
Kotu <phungkevinp@gmail.com>
2025-05-27 11:08:47 -0400

    Imported from workspace

---
+++ src/js/particles-config.js
@@ -0,0 +1,57 @@
+particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 120,
+        "density": {
+          "enable": true,
+          "value_area": 1000
+        }
+      },
+      "color": {
+        "value": "#ffffff" // star color
+      },
+      "shape": {
+        "type": "circle"
+      },
+      "opacity": {
+        "value": 0.9,
+        "random": true,
+        "anim": {
+          "enable": true,
+          "speed": 0.5,
+          "opacity_min": 0.3,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 2,
+        "random": true
+      },
+      "line_linked": {
+        "enable": false // no connecting lines
+      },
+      "move": {
+        "enable": true,
+        "speed": 0.4,
+        "direction": "none",
+        "random": true,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": false
+        },
+        "onclick": {
+          "enable": false
+        },
+        "resize": true
+      }
+    },
+    "retina_detect": true
+  });
+  
